3c7625bc331cc5d38658c16f77db41b4784d6220
noobanon <51384033+noobanon@users.noreply.github.com>
2019-06-20 23:23:48 +0530

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -117,7 +117,7 @@ async def amireallyalive(alive):
         await alive.edit(
             "`"
            "Guruji hum abhi zinda hai ho 😬;) \n\n"
-            f"User: {anonyCrew_sir} \n"
+            f"User: {DEFAULTUSER} \n"
             "`"
             )
 
