85a0f1d01627cce179fbee57e0be51126b478853
baby1483 <52386404+baby1483@users.noreply.github.com>
2019-07-17 09:03:43 +0000

    Update extra.py

--- userbot/modules/extra.py
+++ userbot/modules/extra.py
@@ -46,6 +46,17 @@ async def facepalm(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         await e.edit("🤦‍♂")
 
+@register(outgoing=True, pattern="^.hakii$")
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("███▒▒▒▒▒▒▒50%██████████100%")
+			  
+	for _ in range(32):
+		await asyncio.sleep(0.1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
+
 @register(outgoing=True, pattern="^.moon$")
 async def _(event):
 	if event.fwd_from:
@@ -54,8 +65,8 @@ async def _(event):
 	for _ in range(32):
 		await asyncio.sleep(0.1)
 		await event.edit("".join(deq))
-		deq.rotate(1)
-
+                deq.rotate(1)
+		
 @register(outgoing=True, pattern="^.source$")
 async def source(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
